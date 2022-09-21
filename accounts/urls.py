94f07a450ba8813346402d59d31035621babb24e
george-mountain <georgejiokem@gmail.com>
2022-09-21 17:07:26 +0900

    vendor registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('registerUser/', views.registerUser, name='registerUser')
+    path('registerUser/', views.registerUser, name='registerUser'),
+    path('registerVendor/', views.registerVendor, name='registerVendor'),
    
 ] 
