4dd5d91afe718cec13677c344bffabc6b9cc52dc
george-mountain <georgejiokem@gmail.com>
2022-09-25 08:53:15 +0900

    Restaurant Profile Form & Custom Validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms 
-from .models import User
+from .models import User, UserProfile
+from .validators import allow_only_images_validator
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -22,3 +23,21 @@ class UserForm(forms.ModelForm):
             )
 
 
+class UserProfileForm(forms.ModelForm):
+    # Styling the form of the vprofile page with our custom class
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...','required':'required'}))
+    profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),validators=[allow_only_images_validator])
+    cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}),validators=[allow_only_images_validator])
+
+    # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture','cover_photo','address','country','state','city','pin_code','latitude','longitude']
+
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm,self).__init__(*args, **kwargs)
+        for field in self.fields:
+            if field == 'latitude' or field == 'longitude':
+                self.fields[field].widget.attrs['readonly'] = 'readonly'
--- vendor/views.py
+++ vendor/views.py
@@ -1,4 +1,44 @@
-from django.shortcuts import render
 
+from django.shortcuts import render,get_object_or_404,redirect
+
+from accounts.models import UserProfile
+from vendor.models import Vendor
+from .forms import VendorForm
+from accounts.forms import UserProfileForm
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required, user_passes_test
+from accounts.views import check_role_vendor
+
+
+@login_required
+@user_passes_test(check_role_vendor)
 def vprofile(request):
-    return render(request, 'vendor/vprofile.html')
+    profile = get_object_or_404(UserProfile,user=request.user) # import from django.shortcuts
+    vendor = get_object_or_404(Vendor,user=request.user)
+
+    #checking to store information from the profile form entered by vendor.
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST,request.FILES,instance=profile) 
+        vendor_form = VendorForm(request.POST,request.FILES,instance=vendor) 
+        if profile_form.is_valid() and vendor_form.is_valid():
+            profile_form.save()
+            vendor_form.save()
+            messages.success(request,'Restaurant Profile Update successfully!')
+            return redirect('vprofile')
+        else:
+            print(profile_form.errors)
+            print(vendor_form.errors)
+    
+    else:
+    # passing the instance profile and vendor loads the content of the existing form on our html render
+        profile_form = UserProfileForm(instance=profile) # make this class on the forms.py of account and import here.
+        vendor_form = VendorForm(instance=vendor)  #import this from .forms which is the forms.py of the account
+
+    context = {
+        'profile_form': profile_form,
+        'vendor_form': vendor_form,
+        'profile': profile,
+        'vendor': vendor,
+    }
+    return render(request, 'vendor/vprofile.html',context)
