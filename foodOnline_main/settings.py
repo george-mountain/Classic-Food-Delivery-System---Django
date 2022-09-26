8a433866af147618fce6f34d9b15a9ce68eabd8b
george-mountain <georgejiokem@gmail.com>
2022-09-26 16:44:03 +0900

    Google Autocomplete field implementation

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'accounts.context_processors.get_vendor', #our custom processor
+                'accounts.context_processors.get_google_api',
             ],
         },
     },
@@ -156,3 +157,6 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <engrmountain@gmail.com>'
+
+
+GOOGLE_API_KEY = 'AIzaSyAZ4-8UFWH_vF76IxqctyKCIffPz7zUfus'
