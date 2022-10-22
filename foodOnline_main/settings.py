f22854630ad3cb9b6340d4d527fcf35c9e7e2903
george-mountain <georgejiokem@gmail.com>
2022-10-22 17:42:36 +0900

    run gdal path only on local server

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -175,10 +175,10 @@ GOOGLE_API_KEY = config('GOOGLE_API_KEY')
 
 
 # GDAL Libraries
-
-os.environ['PATH'] = os.path.join(BASE_DIR, 'webappenv\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
-os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'webappenv\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
-GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'webappenv\Lib\site-packages\osgeo\gdal304.dll')
+if DEBUG == True:
+    os.environ['PATH'] = os.path.join(BASE_DIR, 'webappenv\Lib\site-packages\osgeo') + ';' + os.environ['PATH']
+    os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'webappenv\Lib\site-packages\osgeo\data\proj') + ';' + os.environ['PATH']
+    GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'webappenv\Lib\site-packages\osgeo\gdal304.dll')
 
 
 # Paypal credentials
