f7f4de04545bb7e9883c272f9de78bd1525dda28
george-mountain <georgejiokem@gmail.com>
2022-10-22 19:29:09 +0900

    database backup

--- accounts/signals.py
+++ accounts/signals.py
@@ -4,21 +4,21 @@ from .models import User, UserProfile
 
 
 
-@receiver(post_save,sender=User)    
-def post_save_create_profile_receiver(sender,instance,created,**kwargs):
-    print(created)
-    if created:
-        UserProfile.objects.create(user=instance)
-        # print('The user proifle has been created')
-    else:
-        try:
-            profile = UserProfile.objects.get(user=instance)
-            profile.save()
-        except:
-            #create the userprofile if it does not exist before
-            UserProfile.objects.create(user=instance)
-        #     print('Profile did not exist but new one created now')
-        # print('user is updated')
+# @receiver(post_save,sender=User)    
+# def post_save_create_profile_receiver(sender,instance,created,**kwargs):
+#     print(created)
+#     if created:
+#         UserProfile.objects.create(user=instance)
+#         # print('The user proifle has been created')
+#     else:
+#         try:
+#             profile = UserProfile.objects.get(user=instance)
+#             profile.save()
+#         except:
+#             #create the userprofile if it does not exist before
+#             UserProfile.objects.create(user=instance)
+#         #     print('Profile did not exist but new one created now')
+#         # print('user is updated')
 
 @receiver(pre_save,sender=User)
 def pre_save_profile_receiver(sender,instance,**kwargs):
