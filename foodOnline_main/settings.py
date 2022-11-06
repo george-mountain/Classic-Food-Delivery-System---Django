20c74d2f33355e287fca735d5c7778f5b13bbe5e
george_mountain <georgejiokem@gmail.com>
2022-11-06 12:53:28 +0900

    Update settings.py

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -168,7 +168,7 @@ EMAIL_PORT = config('EMAIL_PORT',cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <engrmountain@gmail.com>'
+DEFAULT_FROM_EMAIL = 'foodOnline Marketplace <sample_email@gmail.com>'
 
 
 GOOGLE_API_KEY = config('GOOGLE_API_KEY')
